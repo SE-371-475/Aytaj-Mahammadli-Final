af8cff031fa205bf5673dc4bd09fdeebe0f5391f
Aytaj Mahammadli <amahamma@depaul.edu>
2024-09-24 09:39:43 -0500

    created a new PlayerData.java file and modified Player.java file

--- assignment2/Player.java
+++ assignment2/Player.java
@@ -13,5 +13,17 @@ public class Player {
 
         // printing the number of players in the team
         System.out.println("Number of players in the team: " + teamPlayers.numPlayers);
+
+        // creating an object from PlayerData class
+        PlayerData playerRecords = new PlayerData();
+
+        // getting players' info arry by calling getMyData() method
+        PlayerData[] playerRecordsArr = playerRecords.getMyData();
+
+        // printing each object data of the array
+        for (int i = 0; i < playerRecordsArr.length; i++) {
+            PlayerData eachPlayerObj = playerRecordsArr[i];
+            System.out.println("Player number " + (i + 1) + ": " + eachPlayerObj.a + " "+ eachPlayerObj.b + " " + eachPlayerObj.c);
+        }
     }
 }
