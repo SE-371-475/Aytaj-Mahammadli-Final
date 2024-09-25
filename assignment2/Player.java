4011eddef8c2806d563c13cbdbb1d4bd81a91a38
Aytaj Mahammadli <amahamma@depaul.edu>
2024-09-24 20:05:32 -0500

    added addPlayer() method and SeLinkList head to Player.java file

--- assignment2/Player.java
+++ assignment2/Player.java
@@ -1,10 +1,23 @@
 public class Player {
-    // numPlayers vairable refers to number of players
+    // numPlayers variable refers to the number of players
     public int numPlayers;
 
-    // constructor method 
-    public Player(int teamPlayersCnt){
+    // head points to the initial node 
+    private SeLinkList head;
+
+    // constructor method - modified
+    public Player(int teamPlayersCnt) {
         this.numPlayers = teamPlayersCnt;
+        this.head = null; // we set our initial node as null
+    }
+
+    // addPlayer() method to add 
+    public void addPlayer(int a, int b, int c) {
+        // defining our next node
+        SeLinkList nextNode = new SeLinkList(a, b, c);
+        nextNode.next = head;
+        // updating head by assigning nextNode's value to the initial node
+        head = nextNode;
     }
 
     public static void main(String[] args) {
@@ -17,13 +30,25 @@ public class Player {
         // creating an object from PlayerData class
         PlayerData playerRecords = new PlayerData();
 
-        // getting players' info arry by calling getMyData() method
+        // getting players' info array by calling getMyData() method
         PlayerData[] playerRecordsArr = playerRecords.getMyData();
 
         // printing each object data of the array
         for (int i = 0; i < playerRecordsArr.length; i++) {
             PlayerData eachPlayerObj = playerRecordsArr[i];
-            System.out.println("Player number " + (i + 1) + ": " + eachPlayerObj.a + " "+ eachPlayerObj.b + " " + eachPlayerObj.c);
+            System.out.println("Player number " + (i + 1) + ": " + eachPlayerObj.a + " " + eachPlayerObj.b + " " + eachPlayerObj.c);
+        }
+
+        // TASK 7 - new added parts
+        // in every addPlayer() call, it maintains one node which is linked to another
+        teamPlayers.addPlayer(5, 15, 25);
+        teamPlayers.addPlayer(7, 17, 27);
+
+        // printing each player node in order
+        SeLinkList presentNode = teamPlayers.head; // making our present node head and beginning from this point
+        for (int playerOrder = 1; presentNode != null; playerOrder++) {
+            System.out.println("Number " + playerOrder + " Player: " + presentNode.a + " " + presentNode.b + " " + presentNode.c);
+            presentNode = presentNode.next; // updating our current position in node
         }
     }
 }
